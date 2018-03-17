3d44ef7d550910f2cfbdd9aaa2146502d24085bc
LAPTOP-1693FJKC\jamie <jamie.lees@deeperthanblue.co.uk>
2018-03-17 11:11:42 +0000

    Base google test Base QAL open test

---
+++ Object Repository/Page_BEAUTY PIE/PopUp_Close.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Close pop up displayed on page load</description>
+   <name>PopUp_Close</name>
+   <tag></tag>
+   <elementGuidId>3de1557c-48fb-4189-bdca-b326343fa03a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/div[13]/div[2]/div/div/div/span/div/div[4]/div/div/div[2]</value>
+   </webElementProperties>
+</WebElementEntity>
